b59769fafdcf0cb1fce13aad22dfd1c75e777922
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2023-10-29 18:52:13 +0000

    initial temp completed

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 const RootLayout = () => {
   return (
+    <>
     <div>RootLayout</div>
+    <Link to="/sign-up" >" " sing up</Link>
+    </>
+    
   )
 }
 
